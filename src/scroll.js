0e36438c66d6efd3a6cca656f5ba4527329c7343
Jenna Ransom <jenna.ransom@gmail.com>
2021-07-22 18:00:24 -0600

    adds a scroll progress bar to top of page

--- src/scroll.js
+++ src/scroll.js
@@ -1,10 +1,20 @@
 document.addEventListener('DOMContentLoaded', () =>{
 
   const pixelTag = document.querySelector('div.pixel')
+  const bodyTag = document.querySelector('body')
+  const progressTag = document.querySelector('div.progress')
 
   document.addEventListener('scroll', () => {
     const pixels = window.pageYOffset
     pixelTag.innerHTML = pixels
+    progressBar(pixels)
   })
+
+  function progressBar(pixels) {
+    const pageHeight = bodyTag.getBoundingClientRect().height
+    const totalScrollDistance = pageHeight - window.innerHeight
+    const percentage = pixels/totalScrollDistance * 100
+    progressTag.style.width = `${percentage}%`
+  }
 
 })

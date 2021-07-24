39415cfe0b1057e401aca0a3071ceb68e0240147
Jenna Ransom <jenna.ransom@gmail.com>
2021-07-24 14:58:59 -0600

    updates client name and page count when scroll enters new section

--- src/scroll.js
+++ src/scroll.js
@@ -4,10 +4,15 @@ document.addEventListener('DOMContentLoaded', () =>{
   const bodyTag = document.querySelector('body')
   const progressTag = document.querySelector('div.progress')
 
+  const sections = document.querySelectorAll('section')
+  const clientTag = document.querySelector('div.client')
+  const pageTag = document.querySelector('div.page')
+
   document.addEventListener('scroll', () => {
     const pixels = window.pageYOffset
     pixelTag.innerHTML = pixels
     progressBar(pixels)
+    clientAndPageUpdate(pixels)
   })
 
   function progressBar(pixels) {
@@ -15,6 +20,15 @@ document.addEventListener('DOMContentLoaded', () =>{
     const totalScrollDistance = pageHeight - window.innerHeight
     const percentage = pixels/totalScrollDistance * 100
     progressTag.style.width = `${percentage}%`
+  }
+
+  function clientAndPageUpdate(pixels) {
+    sections.forEach( s => {
+      if (s.offsetTop - 100 <= pixels) {
+        clientTag.innerHTML = s.getAttribute('data-client')
+        pageTag.innerHTML = s.getAttribute('data-page')
+      }
+    })
   }
 
 })

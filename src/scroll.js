3d5dd66487525b5524e6981b80c76e6f2e8b41b0
Jenna Ransom <jenna.ransom@gmail.com>
2021-07-26 18:50:11 -0600

    changes header text and progress bar to white in dark section

--- src/scroll.js
+++ src/scroll.js
@@ -7,6 +7,7 @@ document.addEventListener('DOMContentLoaded', () =>{
   const sections = document.querySelectorAll('section')
   const clientTag = document.querySelector('div.client')
   const pageTag = document.querySelector('div.page')
+  const headerTag = document.querySelector('header')
 
   document.addEventListener('scroll', () => {
     const pixels = window.pageYOffset
@@ -27,6 +28,13 @@ document.addEventListener('DOMContentLoaded', () =>{
       if (s.offsetTop - 100 <= pixels) {
         clientTag.innerHTML = s.getAttribute('data-client')
         pageTag.innerHTML = s.getAttribute('data-page')
+        if (s.hasAttribute("data-dark")) {
+          headerTag.classList.add("white")
+          progressTag.classList.add("white")
+        } else {
+          headerTag.classList.remove("white")
+          progressTag.classList.remove("white")
+        }
       }
     })
   }
